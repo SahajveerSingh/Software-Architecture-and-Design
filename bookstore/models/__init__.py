3f83489cb5a49b0effb376caba2093869ebd051a
Sahajveer Singh <86611679+SahajveerSingh@users.noreply.github.com>
2026-06-07 20:04:15 +1000

    Delete bookstore directory

--- bookstore/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .customer import Customer
-from .book import Book
-from .order import CartItem, OrderItem, Invoice, Order
-
-__all__ = ["Customer", "Book", "CartItem", "OrderItem", "Invoice", "Order"]
